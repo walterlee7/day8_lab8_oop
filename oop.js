bb19fcc378198a5b4b4407f455dfdd7250b98cd6
walterlee <walter.lee9@gmail.com>
2018-01-17 15:50:09 -0600

    complete

---
+++ oop.js
@@ -0,0 +1,72 @@
+let container = document.getElementById('square-container');
+let dice = [];
+
+class Die {
+    constructor(x, y, size) {
+        this.div = document.createElement('div');
+        this.div.classList.add('square');
+        this.div.id = 'sq';
+        this.div.style.left = `${x}px`;
+        this.div.style.top = `${y}px`;
+        this.div.style.width = `${size}px`;
+        this.div.style.height = `${size}px`;
+        this.div.style.backgroundColor = "white";
+        this.div.addEventListener('click', () => {
+            this.roll();
+        })
+        this.div.addEventListener('dblclick', () => {
+            this.remove();
+        })
+        container.appendChild(this.div);
+    }
+
+    roll() {
+        this.value = Math.floor(Math.random() * (7 - 1)) + 1;
+        let numb = `&#${this.value + 9855};`
+        this.div.innerHTML = numb;
+    }
+
+    remove() {
+        this.div.remove();
+        for (let i = 0; i < dice.length; i++) {
+            if (dice[i] === this) {
+                dice.splice(i, 1);
+            }
+        }
+    }
+}
+
+let dieButton = document.getElementById('sq-button');
+dieButton.addEventListener('click', createDie);
+
+function createDie() {
+    let die = new Die(100, 100, 100);
+    die.roll();
+    dice.push(die);
+}
+
+let rollButton = document.getElementById('roll-button');
+rollButton.addEventListener('click', rollDie);
+
+function rollDie() {
+    for (let i = 0; i < dice.length; i++) {
+        dice[i].roll();
+    }
+}
+
+let sumButton = document.getElementById('sum-button');
+sumButton.addEventListener('click', sumDice);
+
+function sumDice() {
+
+    let sum = 0;
+    for (let i = 0; i < dice.length; i++) {
+        sum += dice[i].value;
+    }
+    alert(sum);
+}
+
+
+
+
+
